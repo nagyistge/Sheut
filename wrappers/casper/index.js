d3dc9e6069e493398079222e6415a686237436bf
Peter Mouland <peter.mouland@gmail.com>
2015-06-16 12:34:32 +0100

    add more logging

--- wrappers/casper/index.js
+++ wrappers/casper/index.js
@@ -1,26 +1,23 @@
 var gutil = require('gulp-util');
-var spawn = require('child_process').spawn;
-var Promise = require("es6-promise").Promise;
+var Promise = require('es6-promise').Promise;
+var child_process = require('child_process');
+
+function execPromise(cmd, args){
+    return new Promise(function(resolve, reject){
+        child_process.exec(cmd + ' ' + args.join(' '),
+            function (error, stdout, stderr) {
+                stdout && console.log(stdout);
+                stderr && console.log(stderr);
+                if (error !== null) {
+                    reject( cmd + ' ' + args[0] + ' Error: ' + error);
+                } else {
+                    resolve(cmd + ' ' + args[0] + ' Complete');
+                }
+            });
+    });
+}
 
 module.exports = function casper(command) {
     console.log('Casper Node', command)
-    return new Promise(function(resolve, reject){
-        var cmd = (typeof command === 'undefined') ? 'test' : command;
-        cmd = cmd ? (Array.isArray(cmd) ? cmd : cmd.split(' ')) : [];
-        console.log('Casper CMD', cmd)
-        var casperChild = spawn('casperjs', cmd);
-
-        casperChild.stdout.on('data', function(data) {
-            console.log('Casper data', data)
-            var msg = data.toString().slice(0, -1);
-            gutil.log(msg);
-        });
-
-        casperChild.on('close', function(code) {
-            console.log('Casper close', code)
-            var success = code === 0;
-            success && resolve();
-            !success && reject();
-        });
-    });
+    return execPromise('casperjs', command);
 };
